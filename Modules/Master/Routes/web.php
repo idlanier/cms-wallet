1d821c01e526959788ae88aac553f72d70078eee
idlanier <reinaldi.didi97@gmail.com>
2019-08-21 09:45:06 +0700

    Commit: 1. Penambahan Routes untuk Master Wallet dan Master Category

--- Modules/Master/Routes/web.php
+++ Modules/Master/Routes/web.php
@@ -13,4 +13,25 @@
 
 Route::prefix('master')->group(function() {
     Route::get('/', 'MasterController@index');
+
+    // Route Master Wallet
+    Route::post('/addWallet', 'MasterWalletController@addWallet');
+    Route::get('/getWalletAdvanceList', 'MasterWalletController@getWalletAdvanceList');
+    Route::get('/findWalletById', 'MasterWalletController@findWalletById');
+    Route::post('/editWallet', 'MasterWalletController@editWallet');
+    Route::get('/getWalletStatusList', 'MasterWalletController@getWalletStatusList');
+    Route::get('/getWalletList', 'MasterWalletController@getWalletList');
+    Route::post('/doActiveWalletStatus', 'MasterWalletController@doActiveWalletStatus');
+    Route::post('/doNotActiveWalletStatus', 'MasterWalletController@doNotActiveWalletStatus');
+
+    // Route Master Category
+    Route::post('/addCtgr', 'MasterCategoryController@addCtgr');
+    Route::get('/getCtgrAdvanceList', 'MasterCategoryController@getCtgrAdvanceList');
+    Route::get('/findCtgrById', 'MasterCategoryController@findCtgrById');
+    Route::post('/editCtgr', 'MasterCategoryController@editCtgr');
+    Route::get('/getCtgrStatusList', 'MasterCategoryController@getCtgrStatusList');
+    Route::get('/getWalletList', 'MasterCategoryController@getWalletList');
+    Route::post('/doActiveCtgrStatus', 'MasterCategoryController@doActiveCtgrStatus');
+    Route::post('/doNotActiveCtgrStatus', 'MasterCategoryController@doNotActiveCtgrStatus');
+
 });
