1f426425a9a64b73ede8cf2373755959963e4813
idlanier <reinaldi.didi97@gmail.com>
2019-08-21 18:14:12 +0700

    Commit: 1. Revisi tampilan pada master wallet distandarisasi

---
+++ Modules/Master/Resources/views/wallet/detailWallet.blade.php
@@ -0,0 +1,13 @@
+<div class="page-header">
+    <h1 class="page-title">Detail Wallet</h1>
+</div>
+<div class="page-content">
+    <div class="panel">
+        <div class="panel-heading">
+            <h3 class="panel-title">Detail Wallet</h3>
+        </div>
+        <div class="panel-body">
+                TEST
+        </div>
+    </div>
+</div>
