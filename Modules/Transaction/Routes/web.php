675a96f3fdee0fc8f8dbe526c066f9dddefc2396
idlanier <reinaldi.didi97@gmail.com>
2019-08-21 18:18:58 +0700

    Commit: 1. Penambahan Route View untuk kebutuhan AngularJs pada modul Transaksi Wallet

--- Modules/Transaction/Routes/web.php
+++ Modules/Transaction/Routes/web.php
@@ -14,7 +14,14 @@
 Route::prefix('transaction')->group(function() {
     Route::get('/', 'TransactionController@index');
 
-    // Route Transaction Wallet
+    // Route View Transaction Wallet
+    Route::get('/transaction', 'WalletTransactionController@index');
+    Route::get('/viewAddWalletTransactionIn', 'WalletTransactionController@viewAddWalletTransactionIn');
+    Route::get('/viewAddWalletTransactionOut', 'WalletTransactionController@viewAddWalletTransactionOut');
+    Route::get('/viewWalletAddTransactionIn', 'WalletTransactionController@viewWalletAddTransactionIn');
+    Route::get('/viewWalletAddTransactionOut', 'WalletTransactionController@viewWalletAddTransactionOut');
+
+    // Route Transaction Wallet Repository
     Route::post('/addTrxWalletIn', 'WalletTransactionController@addTrxWalletIn');
     Route::post('/addTrxWalletOut', 'WalletTransactionController@addTrxWalletOut');
     Route::get('/getTrxList', 'WalletTransactionController@getTrxList');
